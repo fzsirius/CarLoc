df2945a0983f640402588051163b075a74a3d414
Karim Tchere <tcherekarim@gmail.com>
2023-04-03 00:05:12 +0200

    modification lien image

--- CarLoc/connexion.php
+++ CarLoc/connexion.php
@@ -57,7 +57,7 @@ if (!empty($_POST)) {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       <link href="assets/css/styles.css" rel="stylesheet" />
       <style>  body{
-       background-image: url('assets/img/a.jpg');
+       background-image: url('images/ab.jpg');
        background-repeat : no-repeat;
        background-size : 100% 100%;
        background-attachment:fixed;
