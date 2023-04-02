b93a1f2fdbe3320b77c35c951fe133861bb6101b
Karim Tchere <tcherekarim@gmail.com>
2023-04-02 23:56:22 +0200

    Mise à jour page connexion.php

--- CarLoc/connexion.php
+++ CarLoc/connexion.php
@@ -1,52 +1,53 @@
-    <?php
-require_once('include.php'); // Charge le fichier include.php
+<?php
+session_start();
+require_once('include.php');
 
-if (!empty($_POST)) { // Vérifie s'il y a des données soumises via la méthode POST
-    extract($_POST); // Extrait les variables de la soumission POST pour que nous puissions les utiliser comme des variables normales
+if (!empty($_POST)) {
+    extract($_POST);
+    $valid = true;
+    $errorClass = 'error-message';
 
-    $valid = true; // Initialise la variable $valid à true
-    $errorClass = 'error-message'; // Initialise la variable $errorClass à 'error-message'
-
-    if (isset($_POST['Connexion'])) { // Vérifie si l'utilisateur a soumis les informations de connexion
-        $email = trim($email); // Supprime les espaces blancs au début et à la fin de la variable $email
-        $mdp = trim($mdp); // Supprime les espaces blancs au début et à la fin de la variable $mdp
+    if (isset($_POST['Connexion'])) {
+        $email = trim($email);
+        $mdp = trim($mdp);
     }
 
-    if (empty($email)) { // Vérifie si la variable email est vide
-        $valid = false; // Définit la variable $valid à false
-        $err_email = "<span class='$errorClass'>Email obligatoire</span>"; // Définit la variable $err_email à un message d'erreur
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) { // Vérifie si la variable $email est une adresse email valide
-        $valid = false; // Définit la variable $valid à false
-        $err_email = "<span class='$errorClass'>Email invalide</span>"; // Définit la variable $err_email à un message d'erreur
+    if (empty($email)) {
+        $valid = false;
+        $err_email = "<span class='$errorClass'>Email obligatoire</span>";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $valid = false;
+        $err_email = "<span class='$errorClass'>Email invalide</span>";
     }
 
-    if (empty($mdp)) { // Vérifie si la variable $mdp est vide
-        $valid = false; // Définit la variable $valid à false
-        $err_mdp = "<span class='$errorClass'>Mot de passe obligatoire</span>"; // Définit la variable $err_mdp à un message d'erreur
+    if (empty($mdp)) {
+        $valid = false;
+        $err_mdp = "<span class='$errorClass'>Mot de passe obligatoire</span>";
     }
 
-    if ($valid) { // Vérifie si toutes les validations ont été passées avec succès
-        // Récupère le mot de passe de l'utilisateur depuis la base de données en utilisant son adresse e-mail
+    if ($valid) {
         $req = $DB->prepare("SELECT * FROM client WHERE Email = ?");
         $req->execute(array($email));
         $user = $req->fetch();
 
-        // Vérifie le mot de passe de l'utilisateur
         if (!$user || !password_verify($mdp, $user['mdp1'])) {
-            // Si le mot de passe est incorrect, définit la variable $valid à false et $err_mdp à un message d'erreur
             $valid = false;
             $err_mdp = "<span class='$errorClass'>Identifiants incorrects</span>";
         } else {
-            // Si l'utilisateur est authentifié, définit la variable de session $_SESSION['user_id'] et redirige vers la page d'accueil
-            $_SESSION['user_id'] = $user['idClient'];
+            $client = $user;
+            $_SESSION['client'] = array(
+                'id' => $client['ID_client'],
+                'nom' => $client['Nom'],
+                'prenom' => $client['Prenom'],
+                'mail' => $client['Email'],
+            );
             header('Location: principal.php');
             exit;
         }
     }
 }
-
-
 ?>
+
 
 <html lang="en">
   <head>
@@ -56,7 +57,7 @@ if (!empty($_POST)) { // Vérifie s'il y a des données soumises via la méthode
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       <link href="assets/css/styles.css" rel="stylesheet" />
       <style>  body{
-       background-image: url('images/a.jpg');
+       background-image: url('assets/img/a.jpg');
        background-repeat : no-repeat;
        background-size : 100% 100%;
        background-attachment:fixed;
@@ -129,7 +130,7 @@ button {
 }
 
 button[type="submit"]:hover {
-background-color: #FFA500;
+background-color: #E30509;
 color: white;
 border: 2px solid #0069d9;
 border-radius: 5px;
