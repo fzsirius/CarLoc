a8a2dbc9a28f28bbdbaac8e79653c8be85a8e15d
Karim Tchere <tcherekarim@gmail.com>
2023-04-02 15:43:27 +0200

    modification du lien vers l'image de fond

--- CarLoc/inscription.php
+++ CarLoc/inscription.php
@@ -87,7 +87,7 @@
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
       <link href="assets/css/styles.css" rel="stylesheet" />
       <style>  body{
-       background-image: url('assets/img/b.jpg');
+       background-image: url('images/a.jpg');
        background-repeat : no-repeat;
        background-size : 100% 100%;
        background-attachment:fixed;
