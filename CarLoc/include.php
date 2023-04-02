c87052943805b0ca627edcd9331be38ed2854cfe
Karim Tchere <tcherekarim@gmail.com>
2023-04-02 15:47:48 +0200

    mise à jour fichier include.php

--- CarLoc/include.php
+++ CarLoc/include.php
@@ -1,3 +1,21 @@
 <?php
 include_once('_db/connexionDB.php');
 ?>
+<?php
+$cncarloc=new MySQLi("localhost","root","","karim_carloc");
+
+?>
+<?php
+// Définir les paramètres de connexion à la base de données
+$host = 'localhost';
+$dbname = 'karim_carloc';
+$username = 'root';
+$password = '';
+
+// Connexion à la base de données
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+} catch (PDOException $e) {
+    die("La connexion à la base de données a échoué: " . $e->getMessage());
+}
+?>
